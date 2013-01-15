8006c6be3624b0556cf75ac0bee8c523c76ae9dd
Tim Cameron Ryan <id@timryan.org>
2013-01-15 06:42:40 -0500

    One step closer to instant boot

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var fs = require('fs')
   , send = require('send')
   , wrench = require("wrench")
   , url = require('url')
-  , UglifyJS = require("uglify-js");
+  , UglifyJS = require("uglify-js")
+  , temp = require('temp');
 
 function codifyJSON (obj) {
   return JSON.stringify(obj).replace(/\*/g, '\\*').replace(/\//g, '\\/');
@@ -11,39 +12,47 @@ function codifyJSON (obj) {
 
 module.exports = function requireware () {
   var bases = Array.prototype.slice.call(arguments);
+  var cache = temp.openSync('requireware'), cached = false;
+
+  function refresh (req) {
+    var scripts = {};
+    bases.forEach(function (base) {
+      var localbase = path.join(base, path.join('/', req.path));
+      wrench.readdirSyncRecursive(localbase)
+        .forEach(function (filename) {
+          // Only accept valid filenames ending in .js
+          if (!filename.match(/\.js$/)) {
+            return;
+          }
+
+          var file = path.join(localbase, filename);
+          if (fs.statSync(file).isFile()) {
+            var localpath = path.join(req.path, filename);
+            var scripturl = (req.header('X-Forwarded-Protocol') || 'http') + '://' + req.header('host') + path.join(url.parse(req.originalUrl).pathname, localpath);
+            var scriptsrc = fs.readFileSync(file, 'utf-8');
+
+            // Syntax check.
+            try {
+              UglifyJS.parse(scriptsrc, {filename: localpath});
+            } catch (e) {
+              scriptsrc = 'throw new SyntaxError(' + codifyJSON(e.message + " at line " + e.line + ", col " + e.col) + ');';
+            }
+            scripts[localpath.replace(/^\//, '')] = '(function () { var exports={}; ' + scriptsrc  + '\nreturn exports; })()\n//@ sourceURL=' + scripturl;
+          }
+        });
+    });
+    fs.writeFileSync(cache.path, 'require && (require.content = (' + codifyJSON(scripts) + ')) && require._isReady(' + JSON.stringify(req.path) + ');', 'utf-8');
+  }
 
   return function (req, res, next) {
     if (req.path.charAt(req.path.length - 1) == '/') {
+      res.setHeader('Content-Type', 'text/javascript');
       if ('source' in req.query) {
-        var scripts = {};
-        bases.forEach(function (base) {
-          var localbase = path.join(base, path.join('/', req.path));
-          wrench.readdirSyncRecursive(localbase)
-            .forEach(function (filename) {
-              // Only accept valid filenames ending in .js
-              if (!filename.match(/\.js$/)) {
-                return;
-              }
-
-              var file = path.join(localbase, filename);
-              if (fs.statSync(file).isFile()) {
-                var localpath = path.join(req.path, filename);
-                var scripturl = (req.header('X-Forwarded-Protocol') || 'http') + '://' + req.header('host') + path.join(url.parse(req.originalUrl).pathname, localpath);
-                var scriptsrc = fs.readFileSync(file, 'utf-8');
-
-                // Syntax check.
-                try {
-                  UglifyJS.parse(scriptsrc, {filename: localpath});
-                } catch (e) {
-                  scriptsrc = 'throw new SyntaxError(' + codifyJSON(e.message + " at line " + e.line + ", col " + e.col) + ');';
-                }
-                scripts[localpath.replace(/^\//, '')] = '(function () { var exports={}; ' + scriptsrc  + '\nreturn exports; })()\n//@ sourceURL=' + scripturl;
-              }
-            });
-        });
-
-        res.setHeader('Content-Type', 'text/javascript');
-        res.send('require && (require.content = (' + codifyJSON(scripts) + ')) && require._isReady(' + JSON.stringify(req.path) + ');');
+        if (!cached) {
+          refresh(req);
+          cached = true;
+        }
+        send(req, cache.path).pipe(res);
       } else {
         send(req, __dirname + '/static.js').pipe(res);
       }
